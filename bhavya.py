d243756548936c37837e12fa12f1e1104a73c3fd
Harsh Raj <95570874+CodeOnIce@users.noreply.github.com>
2024-10-21 22:19:20 +0530

    Update bhavya.py

--- bhavya.py
+++ bhavya.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 
 
 # Configure Gemini API using the API key from environment variable
-genai.configure(api_key="AIzaSyBQIQFhqhphxHqiyu0VDMfkoCc2CEAIW6Y")
+genai.configure(api_key="here")
 
 # Function to interact with the Gemini API and generate a response
 def get_gemini_response(prompt):
