923143c9bdcf242f7df90901a111bc6f74ba9b6a
Harsh Raj <95570874+CodeOnIce@users.noreply.github.com>
2024-11-13 00:13:01 +0530

    Update bhavya.py

--- bhavya.py
+++ bhavya.py
@@ -1,4 +1,4 @@
-import os
+oimport os
 import streamlit as st
 
 import google.generativeai as genai
@@ -44,7 +44,7 @@ if submit:
         with a(n) {activity_level.lower()} activity level. The diet chart should be for a {timeframe.lower()}.
         Ensure that the diet plan provides a balanced intake of proteins, carbohydrates, fats, vitamins, and minerals, 
         considering their age, weight, gender, and activity level. The diet should strictly follow {diet_preference.lower()} dietary guidelines.
-        Include daily meal recommendations with portion sizes and nutritional information for each meal.
+        Include daily meal recommendations with portion sizes and nutritional information for each meal.provide csv file or spreadsheet link 
         """
         
         # Get the response from the AI
